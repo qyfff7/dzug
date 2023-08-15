0963dd08e25f528dfcf46d0b1cc7a149e0f722d5
qkyufw <qkyufw@outlook.com>
2023-08-15 18:53:57 +0800

    favorite模块没有bug版

--- app/gateway/rpc/favorite.go
+++ app/gateway/rpc/favorite.go
@@ -46,32 +46,8 @@ func FavoriteList(ctx context.Context, req *favorite.FavoriteListRequest) (resp 
 		resp.StatusMsg = "获取点赞列表失败"
 		return
 	}
+	// 将resp转为response类型
 
 	resp, err = discovery.FavoriteClient.FavoriteList(ctx, req)
 	return
 }
-
-//type Videos struct {
-//	Id            int64  `json:"id"`             // 视频唯一标识
-//	Author        *Users  `json:"author"`         // 视频作者信息
-//	PlayUrl       string `json:"play_url"`       // 视频播放地址
-//	CoverUrl      string `json:"cover_url"`      // 视频封面地址
-//	FavoriteCount int64  `json:"favorite_count"` // 视频的点赞总数
-//	CommentCount  int64  `json:"comment_count"`  // 视频的评论总数
-//	IsFavorite    bool   `json:"is_favorite"`    // true-已点赞，false-未点赞
-//	Title         string `json:"title"`
-//}
-//
-//type Users struct {
-//	Id              int64  `json:"id"`               // 用户id
-//	Name            string `json:"name"`             // 用户名称
-//	FollowCount     int64  `json:"follow_count"`     // 关注总数
-//	FollowerCount   int64  `json:"follower_count"`   // 粉丝总数
-//	IsFollow        bool   `json:"is_follow"`        // true-已关注，false-未关注
-//	Avatar          string `json:"avatar"`           //用户头像
-//	BackgroundImage string `json:"background_image"` //用户个人页顶部大图
-//	Signature       string `json:"signature"`        //个人简介
-//	TotalFavorited  int64  `json:"total_favorited"`  //获赞数量
-//	WorkCount       int64  `json:"work_count"`       //作品数量
-//	FavoriteCount   int64  `json:"favorite_count"`   //点赞数量
-//}
