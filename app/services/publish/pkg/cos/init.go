800bc4f7da93778e3c31fccc24cf2f33d923e29e
Zhen hua <2509859469@qq.com>
2023-08-30 20:37:53 +0800

    防止bj

--- app/services/publish/pkg/cos/init.go
+++ app/services/publish/pkg/cos/init.go
@@ -13,7 +13,7 @@ func Init() {
 	cosVideo = CosVideo{
 		VideoBucket: "https://byte-camp-1313593665.cos.ap-beijing.myqcloud.com",
 		CoverBucket: "https://cover-1313593665.cos.ap-beijing.myqcloud.com",
-		SecretId:    "AKIDRjofkdIjvtfDXeZJsDLEtVv76tSmskOq",
-		SecretKey:   "HKGxQfgT4RaUAbo9GApGZieMr8C4hu5C",
+		SecretId:    "",
+		SecretKey:   "",
 	}
 }
